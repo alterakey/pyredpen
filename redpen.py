be14452b2649191fcfe8253a5af4389527fe77ee
takahi-i <takahikoito@gmail.com>
2015-04-14 14:47:24 +0900

    Add return code

--- redpen.py
+++ redpen.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import argparse
+import sys
 
 class RedPen:
     def __init__(self):
@@ -47,7 +48,7 @@ class RedPen:
         }
 
     def validate(self):
-        return requests.post('http://redpen.herokuapp.com/rest/document/validate/json', json=self.generate())
+        return requests.post('http://redpen.herokuapp.com/rest/document/validate/json', json=self.generate()).json()
 
 class Builder:
     def __init__(self):
@@ -68,7 +69,17 @@ class Builder:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--doc', action="store", default="sampledoc-en.txt", dest="doc_file")
+    parser.add_argument('--limit', action="store", default=1, dest="limit")
     parse_results = parser.parse_args()
+
     redpen = Builder().add_doc(open(parse_results.doc_file).read()).build()
-    r = redpen.validate()
-    print json.dumps(r.json(), indent=4)
+    result = redpen.validate()
+    if parse_results.limit < len(result["errors"]):
+        print "Found errors more than specified limit..."
+        print len(result["errors"])
+        sys.exit(1)
+    else:
+        print "Succeeded validation"
+        sys.exit(0)
+
+    # print json.dumps(result, indent=4)
