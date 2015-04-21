c7a3e4aeccfc485965f81c9d8f8a39d091538998
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2015-04-21 17:22:58 +0900

    Fixing up breakages

--- flymake.mk
+++ flymake.mk
@@ -1,4 +1,4 @@
 redpendir=/path/to/redpen
 
 check-syntax:
-	for i in $(CHK_SOURCES); do python $(redpendir)/redpen.py --doc=$$i; done
+	(cd $(redpendir) && for i in $(CHK_SOURCES); do python redpen.py --doc=$$i; done)
