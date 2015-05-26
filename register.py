269e3ca944158bde20116e26e6aea49379c389dd
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2015-05-26 14:59:08 +0900

    Renamed

--- register.py
+++
@@ -1,8 +0,0 @@
-import pypandoc
-import os
-
-f = open('README.rst','wb')
-f.write(pypandoc.convert('README.md', 'rst').encode('utf-8'))
-f.close()
-os.system("python ./setup.py register")
-os.remove('README.rst')
